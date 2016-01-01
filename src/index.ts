feb93d0e568e4b67c409deee9eed78b2392c9207
Mike Pleimann <crap944-github@yahoo.com>
2016-01-01 15:18:49 -0600

    imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 'use strict';
 /// <reference path="typings/tsd.d.ts"/>
 
+import { Machine } from './model/machine';
+import { Channel, ChannelType } from './model/channel';
+import { Session } from './model/session';
+
 console.log('hello sleep-data');
