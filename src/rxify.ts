98e164538217deffbcc5cd05b62e1654b12058b9
Mike Pleimann <penguin944@users.noreply.github.com>
2016-01-04 18:25:35 -0600

    Create rxify.ts

---
+++ src/rxify.ts
@@ -0,0 +1,34 @@
+import { Observable, Subscriber } from 'rxjs';
+import * as fs from 'fs';
+
+import { Machine } from './model/machine';
+
+let readFile = fromCallbackFunc(fs.readFile);
+
+readFile("")
+	// .map(jsonMapper)
+	.subscribe(
+		(data: string) => {
+			console.log(data);
+		}, (err: Error) => {
+			console.error(err)
+		}
+	);
+
+}
+
+function fromCallbackFunc<T>(fn: (...args: any[]) => void): (...args: any[]) => Observable<T>{
+	return (...args: any[]): Observable<T> => {
+		return new Observable((subscriber: Subscriber<T>) => {
+			args.push((err: Error, data: T) => {
+				if (err) {
+					subscriber.error(err);
+					
+				} else {
+					subscriber.next(data);
+				}
+			});
+
+			fn.apply(this, args);	
+		});
+	}
