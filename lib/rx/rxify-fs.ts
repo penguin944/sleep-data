f88f6d65a12e62ca6cd63c1337685945bae5fb94
Mike Pleimann <crap944-github@yahoo.com>
2016-01-28 06:16:52 -0600

    update rxify-fs to use Observable.bindNodeCallback

--- lib/rx/rxify-fs.ts
+++ lib/rx/rxify-fs.ts
@@ -1,441 +1,111 @@
 import { Observable, Subject, Subscriber } from 'rxjs';
+import * as Rx from 'rxjs';
 import * as fs from 'fs';
 import * as readline from 'readline';
 
 export default class RxFs {
     public static rename(path1: string, path2: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.rename(path1, path2, handler);
-
-        return subject;
+	    return Observable.bindNodeCallback(fs.rename)(path1, path2);
     }
 
     public static truncate(path: string, len: number): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.truncate(path, len, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.truncate)(path, len);
     }
 
     public static ftruncate(fd: number, len: number): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.ftruncate(fd, len, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.ftruncate)(fd, len);
     }
 
     public static chmod(path: string, mode: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.chmod(path, mode, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.chmod)(path, mode);
     }
 
     public static stat(path: string): Observable<fs.Stats> {
-        let subject = new Subject<fs.Stats>();
-
-        let handler = function(err: NodeJS.ErrnoException, stats: fs.Stats) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(stats);
-                subject.complete();
-            }
-        };
-
-        fs.stat(path, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.stat)(path);
     }
 
     public static lstat(path: string): Observable<fs.Stats> {
-        let subject = new Subject();
-
-        let handler = function(err: NodeJS.ErrnoException, stats: fs.Stats) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(stats);
-                subject.complete();
-            }
-        };
-
-        fs.lstat(path, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.lstat)(path);
     }
 
     public static fstat(fd: number): Observable<fs.Stats> {
-        let subject = new Subject();
-
-        let handler = function(err: NodeJS.ErrnoException, stats: fs.Stats) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(stats);
-                subject.complete();
-            }
-        };
-
-        fs.fstat(fd, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.fstat)(fd);
     }
 
     public static link(srcpath: string, dstpath: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.link(srcpath, dstpath, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.link)(srcpath, dstpath);
     }
 
     public static symlink(srcpath: string, dstpath: string, type?: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.symlink(srcpath, dstpath, type, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.symlink)(srcpath, dstpath, type);
     }
 
     public static readlink(path: string): Observable<string> {
-        let subject = new Subject<string>();
-
-        let handler = function(err: NodeJS.ErrnoException, resolvedPath: string): void {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(resolvedPath);
-                subject.complete();
-            }
-        };
-
-        fs.readlink(path, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.readlink)(path);
     }
 
     public static realpath(path: string): Observable<string> {
-        let subject = new Subject<string>();
-
-        let handler = function(err: NodeJS.ErrnoException, resolvedPath: string): void {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(resolvedPath);
-                subject.complete();
-            }
-        };
-
-        fs.realpath(path, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.realpath)(path);
     }
 
     public static unlink(path: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.unlink(path, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.unlink)(path);
     }
 
     public static rmdir(path: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.rmdir(path, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.rmdir)(path);
     }
 
     public static mkdir(path: string, mode: string): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.mkdir(path, mode, handler);
-
-        return subject;
+		return Observable.bindNodeCallback(fs.mkdir)(path, mode);
     }
 
 	public static readdirFiles(path: string): Observable<string> {
-		let subject = new Subject<string>();
-
-		let handler = function(err: NodeJS.ErrnoException, files: string[]) {
-			if (err) {
-				subject.error(err);
-
-			} else {
-				files.forEach((file: string) => {
-					subject.next(file);
-				});
-
-				subject.complete();
-			}
-		};
-
-		fs.readdir(path, handler);
-
-		return subject;
-
+		return Observable.bindNodeCallback(fs.readdir)(path);
 	}
 
     public static readdir(path: string): Observable<string[]> {
-        return Observable.fromNodeCallback(fs.readdir);
-
-
-        let subject = new Subject<string[]>();
-
-		let handler = function(err: NodeJS.ErrnoException, files: string[]) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(files);
-                subject.complete();
-            }
-        };
-
-        fs.readdir(path, handler);
-
-        return subject;
+        return Observable.bindNodeCallback(fs.readdir)(path);
     }
 
     public static close(fd: number): Observable<void> {
-        let subject = new Subject<void>();
-
-        let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.close(fd, handler);
-
-        return subject;
+        return Observable.bindNodeCallback(rs.close)(fd);
     }
 
     public static open(path: string, flags: string, mode: string): Observable<number> {
-        let subject = new Subject();
-
-		let handler = function(err: NodeJS.ErrnoException, fd: number) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(fd);
-                subject.complete();
-            }
-        };
-
-        fs.open(path, flags, mode, handler);
-
-        return subject;
+        return Observable.bindNodeCallback(fs.open)(path, flags, mode);
     }
 
     public static write(fd: number, buffer: Buffer, offset: number, length: number, position: number):
 		Observable<{ bytesWritten: number, buffer: Buffer }> {
 
-        let subject = new Subject<{ bytesWritten: number, buffer: Buffer }>();
-
-		let handler = function(err: NodeJS.ErrnoException, bytesWritten: number, innerBuffer: Buffer) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next({ bytesWritten: bytesWritten, buffer: innerBuffer });
-                subject.complete();
-            }
-        };
-
-        fs.write(fd, buffer, offset, length, position, handler);
-
-        return subject;
+        return Observable.bindNodecallback(rs.write)(fd, buffer, offset, length, position);
     }
 
     public static read(fd: number, buffer: Buffer, offset: number, length: number, position: number):
 		Observable<{ bytesRead: number, buffer: Buffer }> {
 
-        let subject = new Subject<{ bytesRead: number, buffer: Buffer }>();
-
-		let handler = function(err: NodeJS.ErrnoException, bytesRead: number, innerBuffer: Buffer) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next({ bytesRead: bytesRead, buffer: innerBuffer });
-                subject.complete();
-            }
-        };
-
-        fs.read(fd, buffer, offset, length, position, handler);
-
-        return subject;
+        return Observable.bindNodeCallback(fs.read)(fd, buffer, offset, length, position);
     }
 
     public static readFile(fileName: string, encoding: string): Observable<string> {
-        let subject = new Subject<string>();
-
-		let handler = function(err: NodeJS.ErrnoException, data: string) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(data);
-                subject.complete();
-            }
-        };
-
-        fs.readFile(fileName, encoding, handler);
-
-		return subject;
+        return Observable.bindNodeCallback(fs.readFile)(fs.readFile(fileName, encoding);
     }
 
     public static writeFile(fileName: string, data: string, options?: { encoding?: string; mode?: number; flag?: string; }):
 		Observable<void> {
 
-        let subject = new Subject<void>();
-
-		let handler = function(err?: NodeJS.ErrnoException) {
-            if (err) {
-                subject.error(err);
-
-            } else {
-                subject.next(undefined);
-                subject.complete();
-            }
-        };
-
-        fs.writeFile(fileName, data, options, handler);
-
-		return subject;
+	    return Observable.bindNodeCallback(fs.writeFile)(fileName, data, options);
     }
 
 	public static watch(filename: string, options: { persistent?: boolean; }): Observable<{event: string, filename: string}> {
-		let subject = new Subject<{event: string, filename: string}>();
-
-		let handler = function(event: string, filename: string){
-			subject.next({ event: event, filename: filename });
-		};
-
-		fs.watch(filename, options, handler);
-
-		return subject;
+		return Observable.bindNodeCallback(fs.watch)(filename, options);
 	}
 
     public static watchFile(filename: string, options?: { persistent?: boolean; interval?: number; }):
 		Observable<{curr: fs.Stats, prev: fs.Stats}> {
 
-        let subject = new Subject<{curr: fs.Stats, prev: fs.Stats}>();
-
-        let handler = function(curr: fs.Stats, prev: fs.Stats) {
-            subject.next({ curr: curr, prev: prev });
-        };
-
-        fs.watchFile(filename, options, handler);
-
-		return subject;
+	    return Observable.bindNodeCallback(fs.watchFile)(filename, options);
     }
 
 	/**
